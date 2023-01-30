efc3410790119af25e2c3e45f073ae28bfc69ed9
Bru-Silveira <brunasilveira_adm@outlook.com>
2023-01-30 16:40:51 -0300

    algoritm de Luhn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
 
-console.log(validator);
+let cardNumber = document.getElementById("card-number")
+let button = document.getElementById("btn")
+let message = document.getElementById ("message")
+
+
+button.addEventListener('click', number)
+
+function number(evento) {
+    evento.preventDefault()
+    let numeroCartao = cardNumber.value 
+    let validador = validator.isValid(numeroCartao)
+
+    if (numeroCartao.length < 16) {
+        return(message.innerHTML = "Número de Cartão Incompleto")
+    }
+    else if (validador === true) {
+        return(message.innerHTML = "Cartão Válido")
+    }
+    else if (!validador) {
+        return(message.innerHTML = "Cartão Inválido")
+    }
+    
+}
+
+
+
+
+
+
+
+    
+
+
+
+
