c05d9ce4237d5f841c82fa2ed298dd3aa9479ed9
Bru-Silveira <brunasilveira_adm@outlook.com>
2023-01-31 17:53:48 -0300

    projeto final

--- src/index.js
+++ src/index.js
@@ -1,28 +1,33 @@
 import validator from './validator.js';
 
-let cardNumber = document.getElementById("card-number")
-let button = document.getElementById("btn")
-let message = document.getElementById ("message")
-
+const cardNumber = document.getElementById("card-number")
+const button = document.getElementById("btn")
+const message = document.getElementById ("message")
 
 button.addEventListener('click', number)
 
 function number(evento) {
-    evento.preventDefault()
-    let numeroCartao = cardNumber.value 
-    let validador = validator.isValid(numeroCartao)
-
-    if (numeroCartao.length < 16) {
-        return(message.innerHTML = "Número de Cartão Incompleto")
-    }
-    else if (validador === true) {
-        return(message.innerHTML = "Cartão Válido")
-    }
-    else if (!validador) {
-        return(message.innerHTML = "Cartão Inválido")
-    }
-    
+  evento.preventDefault()
+  const numeroCartao = cardNumber.value 
+  const validador = validator.isValid(numeroCartao)
+  //const digit = validator.maskify(numeroCartao)
+  
+  if (numeroCartao.length < 16) {
+    return(message.innerHTML = "Número de Cartão Incompleto")
+  }
+  else if (validador === true) {
+    return(message.innerHTML = ` Cartão Válido ${validator.maskify(numeroCartao)} `)
+    // $ - retorna uma função, nesse caso está retornando a função do maskify
+    // `crase`- embute as expressões
+  
+  }
+  else if (!validador) {
+    return(message.innerHTML = "Cartão Inválido")
+  }
+  
 }
+
+
 
 
 
